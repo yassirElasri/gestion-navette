2c9282c32b05d936cc014245caa5fad36d1774fe
yassir el-asri <elasriyassirr@gmail.com>
2021-01-07 19:06:15 +0100

    .net 1er V

---
+++ GestionAutocars/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GestionAutocars.MvcApplication" Language="C#" %>
---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿create table utilissateur (id int primary key identity,
+                            nom_complet varchar(30),
+                            email varchar(50),
+                            telephone varchar(30),
+                            login varchar (30),
+                            mdp varchar (30)
+)
+create table car (          id int primary key identity,
+                            societe varchar(30),
+                            maricule varchar(50),
+                            nombre_places int,
+                            
+)
+create table voyage(       
